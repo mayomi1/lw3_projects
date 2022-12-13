fdfa35cc077b88f0c18ee22dc34308e857da60ac
mayomi <ayandiranmayomi@gmail.com>
2022-12-13 07:11:34 +0100

    completed junior

---
+++ RandomWinnerGame/my-app/constants/index.js
@@ -0,0 +1,3 @@
+import abiJSON from '../../hardhat/artifacts/contracts/RandomWinnerGame.sol/RandomWinnerGame.json'
+export const abi = abiJSON.abi;
+export const RANDOM_GAME_NFT_CONTRACT_ADDRESS = '0xA48373666Ab691a9Ed88fFA782c29DB7a630D294'
---
+++ nft-ipfs/my-app/constants/index.js
@@ -0,0 +1,4 @@
+import lw3Punks from '../../hardhat/artifacts/contracts/LW3Punks.sol/LW3Punks.json'
+
+export const NFT_CONTRACT_ADDRESS = "0x8acAb18F24B0dC8Dc2cCD3d490869c45B68989b8";
+export const abi = lw3Punks.abi;
